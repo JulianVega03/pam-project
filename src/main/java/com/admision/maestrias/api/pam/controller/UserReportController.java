aaf9ba5a76c4a91eded68c704a62be12b79e185f
Julian Vega <julianandresbv@ufps.edu.co>
2024-12-10 04:04:37 -0500

    feat: Securizando servicio de descarga reportes en excel

--- src/main/java/com/admision/maestrias/api/pam/controller/UserReportController.java
+++ src/main/java/com/admision/maestrias/api/pam/controller/UserReportController.java
@@ -2,6 +2,7 @@ package com.admision.maestrias.api.pam.controller;
 
 import com.admision.maestrias.api.pam.service.implementations.reports.UserReportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,16 +18,19 @@ public class UserReportController {
     @Autowired
     UserReportService userReportService;
 
+    @Secured("ROLE_ADMIN")
     @GetMapping("/pdf/all")
     public void exportToPdf(HttpServletResponse response) throws IOException {
         this.userReportService.exportToPdf(response);
     }
 
+    @Secured("ROLE_ADMIN")
     @GetMapping("/excel/all")
     public void exportToExcel(HttpServletResponse response) throws IOException {
         this.userReportService.exportToExcel(response);
     }
 
+    @Secured("ROLE_ADMIN")
     @GetMapping("/excel/{id}")
     public void exportToExcelPorUser(@PathVariable("id") Integer id, HttpServletResponse response) throws IOException {
         this.userReportService.exportToExcelPorUser(id, response);
